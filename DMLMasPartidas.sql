832264dedc975f51a1046e62a9be266fffb73b34
ManuelQHernandez <manuel.quinteros@telusinternational.com>
2022-08-11 01:56:29 -0600

    SecondProject update DMLMasPartidas

--- DMLMasPartidas.sql
+++ DMLMasPartidas.sql
@@ -1,5 +1,7 @@
 /**El jugador con más partidas jugadas*/
 
-select max(player.id_user) as Positions, count(player.id_user) as Cantidad_Partidas, usp.name as name 
-from Games player
-join users_player usp on player.id_user = usp.id_user group by player.id_user, usp.name;
+SELECT MAX( COUNT(*)) as Cantidad_partidas
+FROM Games player
+JOIN Users_player usp
+ON player.id_user = usp.id_user 
+GROUP BY player.id_user, usp.name;
